d6c976508ee032c2ef238494b631e554ef4d8a4f
Danil <danilnovikov759@gmail.com>
2020-06-10 10:50:47 +0300

    - Added controllers.

---
+++ Exam/Exam/GroupController.cs
@@ -0,0 +1,54 @@
+﻿using BusinessLogicAbstractions;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using System;
+
+namespace Exam
+{
+    /// <summary>
+    /// Controller for <see cref="User"/>
+    /// </summary>
+    public class GroupController
+    {
+        private readonly IGroupService _rGroupService;
+
+        /// <summary>
+        /// Default ctor
+        /// </summary>
+        /// <param name="groupService"><see cref="IGroupService"/></param>
+        public GroupController(IGroupService groupService)
+        {
+            _rGroupService = groupService ?? throw new ArgumentNullException(nameof(groupService));
+        }
+
+        public IActionResult AddGroup(Group newGroup)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult AddUser(Guid groupId, User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult ChangeGroupForUser(Guid newGroupId, Guid oldGroupId, Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult DeleteGroup(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult UpdateGroup(Group newGroup)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Exam/Exam/UserController.cs
@@ -0,0 +1,49 @@
+﻿using BusinessLogicAbstractions;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using System;
+
+namespace Exam
+{
+    /// <summary>
+    /// Controller for <see cref="User"/>
+    /// </summary>
+    public class UserController
+    {
+        private readonly IUserService _rUserService;
+
+        /// <summary>
+        /// Default ctor
+        /// </summary>
+        /// <param name="userService"><see cref="IUserService"/></param>
+        public UserController(IUserService userService)
+        {
+            _rUserService = userService ?? throw new ArgumentNullException(nameof(userService));
+        }
+
+        public IActionResult AddUser(User newUser)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult DeleteUser(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult GetUserByName(string firstName, string lastName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult UpdateUser(User newUser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
